fe50b4c06a7918ecfc91803565b1f607f0eb62e7
hung55555hung <hung55555hung@gmail.com>
2025-12-17 15:14:34 +0700

    push for test IOS

--- App.tsx
+++ App.tsx
@@ -6,8 +6,7 @@ import { Linking } from 'react-native';
 import Mapbox from '@rnmapbox/maps';
 
 // 🗺️ Set Mapbox Access Token - HARDCODED
-const MAPBOX_TOKEN =
-  'pk.eyJ1IjoiaHVuZzA1NDcyNCIsImEiOiJjbWl5bHk3eWIwaHJoM2dweXBxMGcwMm10In0.gXxq4YOgffAD8vV7y6nSxA';
+const MAPBOX_TOKEN = 'your_mapbox_access_token_here';
 Mapbox.setAccessToken(MAPBOX_TOKEN);
 
 export default function App() {
